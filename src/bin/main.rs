87ced4f39237edffa503efe6b1ad25c0509d2f04
Jacky Zhen <jacky.s.zhen@gmail.com>
2018-07-08 23:53:50 +1200

    minor refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,16 +3,17 @@ extern crate rustian_roulette;
 use rustian_roulette as rr;
 use std::env;
 
+static COUNTDOWN_FROM: usize = 5;
+
 fn main() {
     let config = rr::cmd::Config::new(env::args());
-    let path_str = format!("{}", config.path.display());
     let file = rr::pick_file(config.path.as_path());
     let file = file.as_path();
     let file_str = file.to_str().unwrap();
 
-    rr::printer::print_prob(&path_str, config.chambers);
+    rr::printer::print_prob(&file_str, config.chambers);
     rr::printer::confirmation();
-    rr::printer::count_down(5, file_str);
+    rr::printer::count_down(COUNTDOWN_FROM, file_str);
 
     if rr::fire(file, config.chambers) {
         rr::printer::rest_in_peace(file_str);
